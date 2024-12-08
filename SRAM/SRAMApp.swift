7eb25a8697612c1c92d3f1d0c00ec30151d646d9
SofiaWongg <sofia.el.wong@gmail.com>
2024-12-08 11:45:37 -0500

    Initial Commit

---
+++ SRAM/SRAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRAMApp.swift
+//  SRAM
+//
+//  Created by Sofia Wong on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SRAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
